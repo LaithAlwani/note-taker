a1d9e0c6a18e11ddfede2efa645f3ffc575a0af2
Laith Alwani <laithalwani@gmail.com>
2021-02-01 23:52:45 -0500

    created all routes

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.listen(()=>{
+const notes = [];
+
+app.get('/notes', (req,res)=>{
+    res.sendFile(path.join(__dirname, "views/notes.html"));
+});
+
+app.get('/api/notes', (req,res)=>{
+    return res.json(notes);
+});
+
+app.post('/api/notes', (req,res)=>{
+    res.end('hello from post' )
+});
+
+app.delete('/api/notes/:id', (req,res)=>{
+    res.end("hello from delete");
+});
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+
+app.listen(PORT,()=>{
     console.log(`listening to Port ${PORT}`)
 });
