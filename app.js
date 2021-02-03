43e178e372e68054555b80a0be1a7d56a7f8d758
Laith Alwani <laithalwani@gmail.com>
2021-02-03 00:33:53 -0500

    added a start script

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs   = require('fs');
 // const { json } = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const notes = []
 let id=0;
 app.use(express.urlencoded({extended:true}));
