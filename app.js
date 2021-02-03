b031b7a8380ce7f6435c6fa67db6a5758db2ae2d
Laith Alwani <laithalwani@gmail.com>
2021-02-03 00:53:50 -0500

    fixed writing bug in db.json

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const fs   = require('fs');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const notes = []
-let id=0;
+let notes = [];
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,18 +18,20 @@ app.get('/notes', (req,res)=>{
 app.get('/api/notes', (req,res)=>{
     fs.readFile(__dirname + '/db/db.json', 'utf8', function (err, data) {
         if (err) throw err;
-        data = JSON.parse(data);   
+        data = JSON.parse(data);
+        notes = data;   
+        console.log(notes);
         res.json(data);
       });
     
 });
 
 app.post('/api/notes', (req,res)=>{
-    id++
+    const id = notes.length+ 1;
     const newNote = req.body;
     newNote.id = id; 
     notes.push(newNote);
-    console.log(notes);
+    
     fs.writeFile('./db/db.json', JSON.stringify(notes) , err=>{
         err ? 
         console.error(err) :console.log('saved to database')
