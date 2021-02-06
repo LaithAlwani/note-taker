c2e1813994548f49dafce2a6c6cef667489b8f1b
Laith Alwani <laithalwani@gmail.com>
2021-02-05 23:22:16 -0500

    added comments

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
 const express = require('express');
 const path = require('path');
 const fs   = require('fs');
-const { nextTick } = require('process');
-// const { json } = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 let notes = [];
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/notes', (req,res)=>{
-    res.sendFile(path.join(__dirname, "public/views/notes.html"));
-    
-});
-
-app.get('/api/notes', (req,res)=>{
-    fs.readFile(__dirname + '/db/db.json', 'utf8', function (err, data) {
-        if (err) throw err;
-        data = JSON.parse(data);
-        notes = data;   
-        res.json(data);
-    });
-});
-
-app.post('/api/notes', (req,res)=>{
-    const id = getNextId();
-    const newNote = req.body;
-    newNote.id = id; 
-    notes.push(newNote);
-    saveData();
-    res.end();
-});
-
+// finds the next avaliable id number
 function getNextId(){
     let id = 1;
     notes.forEach(note =>{
@@ -47,6 +19,45 @@ function getNextId(){
     return id;
 }
 
+//writes data to the database
+function saveData() {
+    fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+        err ?
+            console.error(err) : console.log('saved to database');
+    });
+}
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//notes route, sends back the note.html file
+app.get('/notes', (req,res)=>{
+    res.sendFile(path.join(__dirname, "public/views/notes.html"));
+    
+});
+
+//get api notes route, sends back the notes from database
+app.get('/api/notes', (req,res)=>{
+    fs.readFile(__dirname + '/db/db.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        data = JSON.parse(data);
+        notes = data;   
+        res.json(data);
+    });
+});
+
+//post api notes route, sends the notes array to the database 
+app.post('/api/notes', (req,res)=>{
+    const id = getNextId();
+    const newNote = req.body;
+    newNote.id = id; 
+    notes.push(newNote);
+    saveData();
+    res.end();
+});
+
+//api notes deletes a note using it's id number
 app.delete('/api/notes/:id', (req,res)=>{
     const id = parseInt(req.params.id);
     notes.forEach((note, index) =>{
@@ -58,14 +69,8 @@ app.delete('/api/notes/:id', (req,res)=>{
     res.end();
 });
 
-function saveData() {
-    fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
-        err ?
-            console.error(err) : console.log('saved to database');
-    });
-}
-
-app.get('/', (req,res)=>{
+//index route 
+app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, "public/views/index.html"));
 });
 
